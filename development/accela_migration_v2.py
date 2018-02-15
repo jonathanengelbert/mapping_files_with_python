0ca1ea076ab5adbf4805af8c443da23e3f059d41
Jonathan Engelbert <jonathanengelbert@gmail.com>
2018-02-15 13:23:10 -0800

    First production version ready for deployment. Still lives in development folder, will be moved as soon as testing is completed

--- development/accela_migration_v2.py
+++ development/accela_migration_v2.py
@@ -22,30 +22,35 @@ for subdir, dirs, files in os.walk(rootdir):
 #============================================================================
 #Retrieves Path and Document Name
 
+            path = (subdir).replace("\\", "/")
+            doc_name = file
+
+#LOGIC FOR FINDING RECORD NAME ON FILES PRIOR TO 2014
+
             #Test for complete record id in folder name
             record_in_folder_complete_match = re.match(
                 ".*(\d\d\d\d\.(\d)?(\d)?(\d)?(\d))("
-                "[a-zA-Z]([a-zA-Z])?([a-zA-Z])?).*$", file)
+                "([a-zA-Z])?([a-zA-Z])?([a-zA-Z!])).*$", subdir)
 
             #Stores record name from folder name
-            record_in_folder_complete = re.sub(".*(\d\d\d\d\.(\d)?(\d)?(\d)?(\d)."
-                                "[a-zA-Z]([a-zA-Z])?([a-zA-Z])?).*$", "\\1",
-                     file)
+            record_in_folder_complete = re.sub(".*(\d\d\d\d\.(\d)?(\d)?(\d)?(\d)"
+                                "[a-zA-Z!]).*$", "\\1",
+                     subdir)
 
             #Test for complete record id in file name
             record_in_file_complete_match = re.match(
-                ".*(\d\d\d\d\.(\d)?(\d)?(\d)?(\d))("
-                "[a-zA-Z]([a-zA-Z])?([a-zA-Z])?).*$", file)
+                ".*(\d\d\d\d[.](\d)?(\d)?(\d)?(\d))("
+                "[a-zA-Z!]).*$", file)
 
             #Stores record name from file name
 
-            record_in_file_complete = re.sub(".*(\d\d\d\d\.(\d)?(\d)?(\d)?(\d)."
-                                "[a-zA-Z]([a-zA-Z])?([a-zA-Z])?).*$", "\\1",
+            record_in_file_complete = re.sub(".*(\d\d\d\d\.(\d)?(\d)?(\d)(\d)"
+                                "[a-zA-Z!]).*$", "\\1",
                      file)
 
 
             #Test for record id in folder name without letters
-            record_test = re.sub(".*(\d\d\d\d[.](\d)?(\d)?(\d)?(\d)?).*$",
+            record_test = re.sub(".*(\d\d\d\d[.](\d)?(\d)?(\d)(\d)).*$",
                                  "\\1",
                                  subdir)
             #Test for record id in file name
@@ -61,9 +66,6 @@ for subdir, dirs, files in os.walk(rootdir):
                 file_test = "Null"
 
 
-            path = (subdir).replace("\\", "/")
-            doc_name = file
-
 #Handles 1979 and 1981, adding "19" to the record name
 
             if "1979" in subdir or "1981" in subdir:
@@ -74,15 +76,15 @@ for subdir, dirs, files in os.walk(rootdir):
 
                 output_writer.writerow([path, doc_name, record_name])
 
-#Handles records prior to 2014
 
-            elif "1979" not in subdir \
-            and "1981" not in subdir:
-            # and "2016" not in subdir \
-            # and "2015" not in subdir\
-            # and "2016" not in subdir \
-            # and "2017" not in subdir \
-            # and "2018" not in subdir:
+
+            elif "1979" not in subdir\
+            and "1981" not in subdir \
+            and "2014" not in subdir \
+            and "2015" not in subdir\
+            and "2016" not in subdir \
+            and "2017" not in subdir \
+            and "2018" not in subdir:
 
                 if record_in_folder_complete_match != None:
                      record_name = record_in_folder_complete
@@ -104,7 +106,68 @@ for subdir, dirs, files in os.walk(rootdir):
 
                     output_writer.writerow([path, doc_name, record_name])
 
-#Handles Cases
+ # LOGIC FOR FINDING RECORD NAME ON FILES FROM 2014+
+
+            # Test for complete record id in folder name
+            record_in_folder_complete_match = re.match(
+                ".*(\d\d\d\d[-.](\d)(\d)(\d)(\d)(\d)?(\d)?)(\s)?("
+                "([a-zA-Z])([a-zA-Z])([a-zA-Z!])).*$", subdir)
+
+            # Stores record name from folder name
+            record_in_folder_complete = re.sub(
+                ".*(\d\d\d\d[-.](\d)(\d)(\d)(\d)(\d)?(\d)?(\s)?("
+                "([a-zA-Z])([a-zA-Z])([a-zA-Z!]))).*$", "\\1", subdir)
+
+            # Test for complete record id in file name
+            record_in_file_complete_match = re.match(
+                 ".*(\d\d\d\d[-.](\d)(\d)(\d)(\d)(\d)?(\d)?)("
+                "([a-zA-Z])([a-zA-Z])?([a-zA-Z!])?).*$", file)
+
+            # Stores record name from file name
+
+            record_in_file_complete = re.sub(
+                 ".*(\d\d\d\d[-.](\d)(\d)(\d)(\d)(\d)?(\d)?("
+                "([a-zA-Z])([a-zA-Z])?([a-zA-Z!])?)).*$", "\\1", file)
+
+#============================================================================
+
+#PROVISORY (CURRENTLY NOT BEING USED)
+            # Test for record id in folder name without letters
+            record_test = re.sub(".*(\d\d\d\d[.-](\d)(\d)(\d)(\d)(\d)(\d)).*$",
+                                 "\\1",
+                                 subdir)
+            # Test for record id in file name incomplete
+            file_match = re.match(".*((\d)?(\d)?\d\d[.](\d)?(\d)?(\d)?(\d))("
+                                  "[a-zA-Z]).*$", file)
+
+            if file_match != None:
+                file_test = re.sub(".*((\d)?(\d)?\d\d[.](\d)?(\d)?(\d)?(\d))("
+                                   "[a-zA-Z]).*$",
+                                   "\\8",
+                                     file)
+#============================================================================
+
+# Handles records from 2014+
+
+            if "2014" in subdir \
+            or "2015" in subdir \
+            or "2016" in subdir \
+            or "2017" in subdir \
+            or "2018" in subdir:
+                if record_in_folder_complete_match != None:
+                    record_name = record_in_folder_complete
+
+                    output_writer.writerow([path, doc_name, record_name])
+
+                elif record_in_file_complete_match != None:
+                    record_name = record_in_file_complete
+
+                    output_writer.writerow([path, doc_name, record_name])
+
+                else:
+                    record_name = "NULL"
+
+                    output_writer.writerow([path, doc_name, record_name])
 
 #============================================================================
 #Closes CSV file
